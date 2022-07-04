3fedb6a066aedb201554ab294a99c813b9386390
TheHabibi <celebialieren@gmail.com>
2022-07-04 12:20:22 +0200

    Initial Commit

---
+++ Box Buddies/Box_BuddiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Box_BuddiesApp.swift
+//  Box Buddies
+//
+//  Created by Ali Eren Çelebi on 29.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Box_BuddiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Box Buddies/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Box Buddies
+//
+//  Created by Ali Eren Çelebi on 29.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
